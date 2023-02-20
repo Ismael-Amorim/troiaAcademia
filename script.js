f8f5f08dca167712a08b4ac83e4e580a07172f3e
Ismael Amorim <103977244+Ismael-Amorim@users.noreply.github.com>
2023-02-20 13:06:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var btn = document.querySelector('#esconder-mostrar')
 var container = document.querySelector('.container')
 
 btn.addEventListener('click', function () {
-    if (container.style.display === 'block') {
-        container.style.display = 'none'
-    } else {
+    if (container.style.display === 'none') {
         container.style.display = 'block'
+    } else {
+        container.style.display = 'none'
     }
 })
